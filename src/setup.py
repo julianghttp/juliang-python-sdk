f73e98ceb00af36eb04467353632e258c175e470
symbol <xingcxb@hotmail.com>
2021-10-25 11:35:04 +0800

    更新API URL

--- src/setup.py
+++ src/setup.py
@@ -18,7 +18,7 @@ URL = 'https://gitee.com/juliangip/juliang-python-sdk'
 EMAIL = 'juliangip@163.com'
 AUTHOR = 'juliangip'
 REQUIRES_PYTHON = '>=3.9.0'
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
