c10c5043e1b9989520d0bd6ef60c296a8cb7cc74
symbol <xingcxb@hotmail.com>
2021-10-25 11:33:54 +0800

    更新API URL

--- src/juliangip/enums/URL.py
+++ src/juliangip/enums/URL.py
@@ -6,22 +6,22 @@ class URL(Enum):
     # 主站地址
     DOMAIN = "https://v1.api.juliangip.com"
     # 获取账户余额
-    USERS_GETBALANCE = DOMAIN + "/v1/users/getbalance"
+    USERS_GETBALANCE = DOMAIN + "/users/getbalance"
     # 动态代理 - - 提取动态代理
-    DYNAMIC_GETIPS = DOMAIN + "/v1/dynamic/getips"
+    DYNAMIC_GETIPS = DOMAIN + "/dynamic/getips"
     # 动态代理 - - 校验IP可用性
-    DYNAMIC_CHECK = DOMAIN + "/v1/dynamic/check"
+    DYNAMIC_CHECK = DOMAIN + "/dynamic/check"
     # 动态代理 - - 设置代理IP白名单
-    DYNAMIC_SETWHITEIP = DOMAIN + "/v1/dynamic/setwhiteip"
+    DYNAMIC_SETWHITEIP = DOMAIN + "/dynamic/setwhiteip"
     # 动态代理 - - 获取IP白名单
-    DYNAMIC_GETWHITEIP = DOMAIN + "/v1/dynamic/getwhiteip"
+    DYNAMIC_GETWHITEIP = DOMAIN + "/dynamic/getwhiteip"
     # 动态代理 - - 获取代理剩余可用时长
-    DYNAMIC_REMAIN = DOMAIN + "/v1/dynamic/remain"
+    DYNAMIC_REMAIN = DOMAIN + "/dynamic/remain"
     # 动态代理 - - 获取剩余可用时长
-    DYNAMIC_BALANCE = DOMAIN + "/v1/dynamic/balance"
+    DYNAMIC_BALANCE = DOMAIN + "/dynamic/balance"
     # 独享代理 - - 获取代理详情
-    ALONE_GETIPS = DOMAIN + "/v1/alone/getips"
+    ALONE_GETIPS = DOMAIN + "/alone/getips"
     # 独享代理 - - 设置代理IP白名单
-    ALONE_SETWHITEIP = DOMAIN + "/v1/alone/setwhiteip"
+    ALONE_SETWHITEIP = DOMAIN + "/alone/setwhiteip"
     # 独享代理 - - 获取代理IP白名单
-    ALONE_GETWHITEIP = DOMAIN + "/v1/alone/getwhiteip"
+    ALONE_GETWHITEIP = DOMAIN + "/alone/getwhiteip"
